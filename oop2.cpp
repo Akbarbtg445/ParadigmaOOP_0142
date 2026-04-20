d2ec0ae0e08b871fa0d47916e96e8c6b33f4fbae
M.Ridho Akbar <akbarbtg445@gmail.com>
2026-04-20 10:53:07 +0700

    Membuat method

--- oop2.cpp
+++ oop2.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
 using namespace std;
-
+class kategori {
+public:
+string Elektronik;
+string NonElektronik;};
 class barang {
+    public:
 string nama;
 int jumlah;
-string kategori;
+string Kategori;
 string tanggalproduksi;
+void printData(){
+ cout << "Nama:" <<   nama << endl;
+ cout << "Jumlah:" <<   jumlah << endl;
+ cout << "Tanggalproduksi:" <<   tanggalproduksi << endl;
+ cout << "kategori:" <<   Kategori << endl;
+}
 };
