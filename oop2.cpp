d77dc75679953923757c9a60d2a00bd580c5d482
M.Ridho Akbar <akbarbtg445@gmail.com>
2026-04-20 11:29:01 +0700

    selesai

--- oop2.cpp
+++ oop2.cpp
@@ -1,29 +1,27 @@
 #include <iostream>
 using namespace std;
-class kategori {
-public:
-string Elektronik;
-string NonElektronik;};
+
 class barang {
-    public:
-string nama;
-int jumlah;
-string Kategori;
-string tanggalproduksi;
-void printData(){
- cout << "Nama:" <<   nama << endl;
- cout << "Jumlah:" <<   jumlah << endl;
- cout << "Tanggalproduksi:" <<   tanggalproduksi << endl;
- cout << "kategori:" <<   Kategori << endl;
-}
+public:
+    string nama;
+    int jumlah;
+    string tanggalproduksi;
+    string Kategori;
+
+    void printData(){
+        cout << "Nama:" <<   nama << endl;
+        cout << "Jumlah:" << jumlah << endl;
+        cout << "Tanggalproduksi:" <<   tanggalproduksi << endl;
+        cout << "Kategori:" << Kategori << endl;
+    }
 };
 
 int main (){
     barang b;
-    kategori k;
     b.nama = "HP";
     b.jumlah = 2;
     b.tanggalproduksi = "januari";
-    k.Elektronik;
+    b.Kategori = "Elektronik";
 
+    b.printData();
 }
